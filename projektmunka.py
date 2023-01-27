bcf1f36c89eacee06d47dd523fafd0cbaf2311eb
daviddioszegi <105877271+daviddioszeg@users.noreply.github.com>
2023-01-27 15:25:12 +0100

    Create projektmunka.py

---
+++ projektmunka.py
@@ -0,0 +1,7 @@
+szám1=int(input('Kérem az első számod:'))
+szám2=int(input('Kérem a második számodat:'))
+
+if szám1 > szám2:
+    print('Ez a szám a nagyobbik:',szám1)
+else:
+    print('Ez a szám a nagyobbik:',szám2)
